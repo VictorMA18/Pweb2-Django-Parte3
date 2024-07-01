657b6c4d441e236fcc914a1f0874402107ca8d8c
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-01 09:47:10 -0500

    Iniciando con el repositorio

---
+++ destino/src/inicio/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def myHomeView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request,"home.html",{})
+
+def anotherView(request):
+    context = {'mensaje': 'Victor'}
+    return render(request, 'reemplazame.html', context)
